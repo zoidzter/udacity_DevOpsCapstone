269bfbabf7e25e35226c8e7196ce33aafeed0561
EC2 Default User <ec2-user@ip-172-31-5-188.ec2.internal>
2021-12-18 05:37:31 +0000

    Inital commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return f"Simple Flask Webapp for Devops Capstone by Patrick Mallon. Time: {now}"
+            
+app.run(host="0.0.0.0", port=8000, debug=True)
